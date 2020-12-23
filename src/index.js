9b2a81a6b97d63b5e3e1ae514abde4c51ba45332
Wingseter <wingseter@gmail.com>
2020-12-24 02:57:50 +0900

    간단한 디자인 추가

--- src/index.js
+++ src/index.js
@@ -43,12 +43,17 @@ class BookNoteForm extends React.Component{
     // 입력 창 표시
     render() {
         return (
-            <div style={styles.form}>
-                책 제목: <br />
-                <input type='text' value={this.state.book} onChange={e => this.bookChanged(e)}/> <br />
-                노트: <br />
-                <input type='text' value={this.state.note} size = '60' onChange={e => this.noteChanged(e)} /> <br/>
-                <button onClick={e=> this.post()}>저장</button>
+            <div class="shadow-sm p-3 mb-5 bg-white rounded">
+                <div class="input-group mb-3">
+                    <span class="input-group-text inputTitle" id="basic-addon3">책 제목</span>
+                    <input type='text' class="form-control" id="basic-addon3" value={this.state.book} onChange={e => this.bookChanged(e)}/> <br />
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text inputTitle" id="basic-addon3">노트</span>
+                    <input type='text' aria-describedby="basic-addon3" class="form-control" id="basic-addon3" value={this.state.note} size = '60' onChange={e => this.noteChanged(e)} /> <br/>
+                </div>
+
+                <button type="button" class="btn btn-primary" onClick={e=> this.post()}>저장</button>
             </div>
         );
     }
@@ -80,37 +85,20 @@ class BookNoteApp extends React.Component {
 
     render() {
         const itemsHtml = this.state.items.map(e => (
-            <li key={e._id}>{e.book} - {e.note}</li>
+            <li class="list-group-item" key={e._id}>{e.book} - {e.note}</li>
         ));
         return (
             <div>
-                <h1 style={styles.h1}>독서노트</h1>
+                <h1 class="display-3">독서노트</h1>
                 <BookNoteForm onPost={e => this.loadLogs()} />
-                <p style={styles.right}>
-                    <button onClick={e =>this.loadLogs()}>
+                <p class="right">
+                    <button type="button" class="btn btn-primary" onClick={e =>this.loadLogs()}>
                         새로고침
                     </button>
                 </p>
-                <ul>{itemsHtml}</ul>
+                <ul class="list-group">{itemsHtml}</ul>
             </div>
         ) 
-    }
-}
-
-const styles = {
-    h1: {
-        backgroundColor: 'blue',
-        color: 'white',
-        fortSize: 24,
-        padding: 12
-    },
-    form: {
-        padding: 12,
-        border: '1px solid silver',
-        backgroundColor: '#F0F0F0'
-    },
-    right: {
-        textAlign: 'right'
     }
 }
 
